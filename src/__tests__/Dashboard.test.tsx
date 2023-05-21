500de8c8c63f8facf71d1f5f00c60c3959100944
Tests <michalisouroumis@gmail.com>
2023-05-21 14:14:03 +0200

    fix lint errors

--- src/__tests__/Dashboard.test.tsx
+++ src/__tests__/Dashboard.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MemoryRouter, Route, Routes } from 'react-router-dom'
+import { MemoryRouter } from 'react-router-dom'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { AppRoutes } from '../AppRouter'
