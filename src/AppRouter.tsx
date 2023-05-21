4d15ee2883f6addeadd36893d7234625a6043c76
Tests <michalisouroumis@gmail.com>
2023-05-21 14:57:51 +0200

    redirect to login when is not authenticated

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 
 export function AppRoutes() {
+  const isAuthenticated = false
+
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/home" element={<Homepage />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}
+      />
     </Routes>
   )
 }
