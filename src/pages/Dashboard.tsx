cce1af22f128c7c133e7cc8e3c5053c5654271b5
Tests <michalisouroumis@gmail.com>
2023-05-21 18:57:29 +0200

    implemented log out, and it's test

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { logout } from '../redux/slices/authSlice'
 
 function Dashboard() {
+  const dispatch = useDispatch()
+  const handleLogout = () => {
+    dispatch(logout())
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <button type="button" onClick={handleLogout}>
+        Log out
+      </button>
     </div>
   )
 }
